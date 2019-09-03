e75bd1c702511f1a32dc6ddd9e215d3e20b01363
Eugeni Bejan <ebejan@iesjoandaustria.org>
2019-09-03 13:35:06 +0200

    Use the utils functions for check and import controllers

--- package/lib/Server.js
+++ package/lib/Server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-
+const {hasControllersFolder, loadController} = require('./utils')
 //Later we check if the controlelr has this methods
 //if the class implements this methods, we register the route
 // for example if the class has only get and post method, the resource will response only for post and get requests
@@ -11,22 +11,27 @@ let METHODS = ['get','post', 'put', 'delete', 'connect', 'options', 'trace', 'pa
 class Server{
     constructor(ExpressApp){
         this.app = ExpressApp;
+        hasControllersFolder()
     }
     /**
      * @param {String} path The route path
-     * @param {Class} Controller The class that control this router
+     * @param {String} ControllerName The class that control this router
      * @param {Object} config Object with config options
      * @returns Server
      */
-    addRoute(path, Controller, config={}){
+    addRoute(path, ControllerName, config={}){
         if(!this.app) {
             throw Error("The app was not created, call create method for initialize the app")
         }
-
+        
         try {
-            let ctrl = new Controller()
-            if(ctrl.config) { // If the controller has config object to apply it
-                let {config} = ctrl;
+            let Controller =  loadController(ControllerName)
+            if(!Controller) {
+                throw Error("The " + Controller  + " controller not found inside controllers folder")
+            }
+            Controller = new Controller()
+            if(Controller.config) { // If the controller has config object to apply it
+                let {config} = Controller;
                 //See if middleware is set
                 if(config.middleware) {
                     if(Array.isArray(config.middleware)) {
@@ -36,8 +41,8 @@ class Server{
             }
 
             METHODS.forEach((method) => {
-                if(ctrl[method]){
-                    this.app[method](path, ctrl[method]) // Registry the path, method and they controller
+                if(Controller[method]){
+                    this.app[method](path, Controller[method]) // Registry the path, method and they controller
                 }
             })
             return this;
