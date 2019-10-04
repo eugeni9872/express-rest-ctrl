8e43d55b2e2383214c7a7f6f988484992798c584
Eugeni Bejan <ebejan@iesjoandaustria.org>
2019-10-04 14:06:29 +0200

    Check if exported model is an object or a function(class)

--- package/lib/utils.js
+++ package/lib/utils.js
@@ -1,5 +1,11 @@
-const fs = require('fs')
-const CTRL_FOLDER = `${process.cwd()}/controllers/`
+const fs = require('fs');
+const {
+    isObject,
+    makePrototype
+} = require('./helpers');
+const CTRL_FOLDER = `${process.cwd()}/controllers/`;
+
+makePrototype()
 
 /**
  * @description Check if the controllers folder has been create
@@ -7,8 +13,8 @@ const CTRL_FOLDER = `${process.cwd()}/controllers/`
  * @returns void
  */
 hasControllersFolder = function() {
-    if(!fs.existsSync(CTRL_FOLDER)){
-        throw Error("The controllers folder not exist inside root project.")
+    if (!fs.existsSync(CTRL_FOLDER)) {
+        throw Error("The controllers folder not exist inside root project.");
     }
 }
 
@@ -17,58 +23,71 @@ hasControllersFolder = function() {
  * @returns The controller class or false if no controller found
  */
 loadController = function(controllerName) {
-       let ctrl = findController(controllerName, getAllControllers(CTRL_FOLDER))
-       let ctrlClass = require(ctrl)
-       return {ctrl: ctrlClass, method: getMethodName(controllerName)}
+    let ctrl = findController(controllerName, getAllControllers(CTRL_FOLDER));
+    let ctrlClass = require(ctrl);
+    if (!ctrlClass) {
+        throw Error();
+    }
+
+    return {
+        ctrl: getByTypeClass(ctrlClass, controllerName),
+        method: getMethodName(controllerName)
+    };
 }
 
-var findController = function(ctrlName, controllers){
-    let ctrl = controllers.find(function(controller){
-        return getName(controller.name) === getName(ctrlName)
-    })
-    if(!ctrl){
-        throw Error("No controller named " + ctrlName + " was found")
+var getByTypeClass = function(cls, name) {
+    return isObject(cls) === true ? cls[String(getName(name)).capitalize()]: cls;
+}
+
+var findController = function(ctrlName, controllers) {
+    let ctrl = controllers.find(function(controller) {
+        return getName(controller.name) === getName(ctrlName);
+    });
+    if (!ctrl) {
+        throw Error("No controller named " + ctrlName + " was found");
     }
-    return ctrl.path
+    return ctrl.path;
 }
 
 var getName = function(fullCtrlName) {
     try {
-        return fullCtrlName.split('::')[0].trim().toLowerCase()
+        return fullCtrlName.split('::')[0].trim().toLowerCase();
     } catch (error) {
-        throw Error("No controller named " + fullCtrlName + " was found")
+        throw Error("The controller " + fullCtrlName + " has a invalid name");
     }
 }
 
 var getMethodName = function(fullCtrlName) {
     try {
-        return fullCtrlName.split('::')[1].trim().toLowerCase()
+        return fullCtrlName.split('::')[1].trim().toLowerCase();
     } catch (error) {
-        throw Error("No controller named " + fullCtrlName + " was found")
+        throw Error("The controller " + fullCtrlName + " has a invalid name");
     }
 }
 
 
-var makeFileName = function(file){
-    return file.split('.')[0].trim().toLowerCase()
+var makeFileName = function(file) {
+    return file.split('.')[0].trim().toLowerCase();
 }
 
 // List all files in a directory in Node.js recursively in a synchronous fashion
 //https://gist.github.com/kethinov/6658166#gistcomment-1603591
 var getAllControllers = function(dir, filelist) {
     var fs = fs || require('fs'),
-    files = fs.readdirSync(dir);
+        files = fs.readdirSync(dir);
     filelist = filelist || [];
     files.forEach(function(file) {
-      if (fs.statSync(dir + '/' + file).isDirectory()) {
-        filelist = walkSync(dir + '/' + file, filelist);
-      }
-      else {
-        filelist.push({name: makeFileName(file), path:dir + '/'+ file});
-      }
+        if (fs.statSync(dir + '/' + file).isDirectory()) {
+            filelist = walkSync(dir + '/' + file, filelist);
+        } else {
+            filelist.push({
+                name: makeFileName(file),
+                path: dir + '/' + file
+            });
+        }
     });
     return filelist;
-  };
+};
 
 module.exports = {
     hasControllersFolder,
